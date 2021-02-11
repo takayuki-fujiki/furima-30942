430e4e3196b5069112d11bbbe65af7731c2a81d9
takayuki-fujiki <73503020+takayuki-fujiki@users.noreply.github.com>
2021-02-11 11:20:23 +0900

    商品詳細ページの実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_24_131221) do
+ActiveRecord::Schema.define(version: 2021_02_09_223242) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,15 @@ ActiveRecord::Schema.define(version: 2021_01_24_131221) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "purchaseditems", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "item_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_purchaseditems_on_item_id"
+    t.index ["user_id"], name: "index_purchaseditems_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
@@ -68,4 +77,6 @@ ActiveRecord::Schema.define(version: 2021_01_24_131221) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "purchaseditems", "items"
+  add_foreign_key "purchaseditems", "users"
 end
