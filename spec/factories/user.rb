83b2114167305cea3c8918b8abaf82055145f5b9
takayuki-fujiki <73503020+takayuki-fujiki@users.noreply.github.com>
2021-03-09 12:04:54 +0900

    商品購入機能の修正
    単体テスト用ユーザ生成コードを修正

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { 'test' }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { 'Test1234' }
     password_confirmation { password }
     last_name_JP          { '試験' }
     first_name_JP         { '試験' }
