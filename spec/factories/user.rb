98adbda927ee769387f561c983af844ecf27922e
takayuki-fujiki <73503020+takayuki-fujiki@users.noreply.github.com>
2021-03-09 12:06:51 +0900

    商品購入機能の修正
    単体テスト用ユーザのパスワード生成をベタ書きからFakerとベタ書き混合に修正

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { 'test' }
     email                 { Faker::Internet.free_email }
-    password              { 'Test1234' }
+    password              { 'T1' + Faker::Internet.password(min_length: 4) }
     password_confirmation { password }
     last_name_JP          { '試験' }
     first_name_JP         { '試験' }
